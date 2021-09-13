469b8acb1ea25f5e809ee01adca97de6a4f96cb4
Asaf <63059015+AsafSGitHub@users.noreply.github.com>
2021-09-13 11:25:16 +0300

    semi working code in one file

--- mono_tum.cc
+++ mono_tum.cc
@@ -29,6 +29,11 @@
 #include <Converter.h>	
 #include <thread>
 #include "ctello.h"
+#include <istream>
+#include <string>
+#include <vector>
+#include <math.h>
+#include <cmath>
 
 // usings:
 using namespace std;
@@ -41,13 +46,18 @@ using cv::waitKey;
 // function declarations:
 void GetFrames();
 void ScanRoom();
+cv::Mat rot2euler(const cv::Mat & rotationMatrix);
 void StoreMap(ORB_SLAM2::System &SLAM);
+void NavigateToDoor();
+vector<std::string> readCSVRow(const std::string&);
+vector<vector<double>> readCSV(std::istream&);
+double findDoor(std::istream&);
 
 // global variables:
 const char* const TELLO_STREAM_URL{"udp://0.0.0.0:11111"};
 Tello tello{};
-cv::Mat frame;
-std::atomic<bool> isScanning, beginFrame, streamOn;
+cv::Mat frame, euler;
+std::atomic<bool> isScanning, beginFrame, streamOn, moving, startRotating;
 // isScanning - for transfering the images to the orbslam only while scanning the room.
 // beginFrame - to make sure orbslam is initialized only after we begin scanning and images are available.
 // streamOn - to make sure to land only after the stream is off.
@@ -66,6 +76,9 @@ int main(int argc, char **argv)
     isScanning = true;
     beginFrame = false;
     streamOn = true;
+    moving = true;
+    startRotating = false;
+    cv::Mat mat;
 
     thread th1 (GetFrames);
     thread th2 (ScanRoom);
@@ -75,13 +88,23 @@ int main(int argc, char **argv)
 
     while (isScanning)
     {
-        if(!frame.empty())
-            SLAM.TrackMonocular(frame, 0.1);
+        if(!frame.empty()){
+            mat = SLAM.TrackMonocular(frame, 0.1);
+            if (!mat.empty())
+            {
+                startRotating = true;
+                mat = mat.clone().inv();
+                euler = rot2euler(mat);
+            }
+        }
+        else
+            cout << "no frame" << endl;
     }
     
     StoreMap(SLAM);
     SLAM.Shutdown();
     SLAM.SaveKeyFrameTrajectoryTUM("KeyFrameTrajectory.txt");
+    cout << euler << endl;
     return 0;
 }
 
@@ -110,16 +133,18 @@ void GetFrames()
 // responsible for spinning the drone
 void ScanRoom()
 {
-    int degree = 20;
-    while (!beginFrame){}
+    int degree = 24;
+    while (!beginFrame){ sleep(0.2);}
     
     tello.SendCommand("takeoff");
     while (!tello.ReceiveResponse()) { sleep(0.2);}
     sleep(5);
+
+    while (!startRotating){ sleep(0.2);}
     for (int i = degree; i <= 360; i+=degree)
     {
         // cout << "rotatingforward" << endl;
-        tello.SendCommand("cw 20");
+        tello.SendCommand("cw 24");
         while (!tello.ReceiveResponse()) { sleep(0.2);}
         sleep(0.5);
         tello.SendCommand("up 20");
@@ -131,12 +156,53 @@ void ScanRoom()
     }
     isScanning = false;
 
-    while(streamOn){}
+    while(streamOn){ sleep(0.2);}
+    while(moving){ sleep(0.2);}
     tello.SendCommand("land");
     while (!tello.ReceiveResponse()) { sleep(0.2);}
 }
 
-// responsible for spinning the drone
+// Converts a given Rotation Matrix to Euler angles
+cv::Mat rot2euler(const cv::Mat & rotationMatrix)
+{
+    cv::Mat euler(3,1,CV_64F);
+
+    double m00 = (double)rotationMatrix.at<float>(0,0);
+    double m02 = (double)rotationMatrix.at<float>(0,2);
+    double m10 = (double)rotationMatrix.at<float>(1,0);
+    double m11 = (double)rotationMatrix.at<float>(1,1);
+    double m12 = (double)rotationMatrix.at<float>(1,2);
+    double m20 = (double)rotationMatrix.at<float>(2,0);
+    double m22 = (double)rotationMatrix.at<float>(2,2);
+
+    double x, y, z;
+
+    // Assuming the angles are in radians.
+    if (m10 > 0.998) { // singularity at north pole
+        x = 0;
+        y = CV_PI/2;
+        z = atan2(m02,m22);
+    }
+    else if (m10 < -0.998) { // singularity at south pole
+        x = 0;
+        y = -CV_PI/2;
+        z = atan2(m02,m22);
+    }
+    else
+    {
+        x = atan2(-m12,m11);
+        y = asin(m10);
+        z = atan2(-m20,m00);
+    }
+
+    euler.at<double>(0) = (double)((x/CV_PI) * 180);
+    euler.at<double>(1) = (double)((y/CV_PI) * 180);
+    euler.at<double>(2) = (double)((z/CV_PI) * 180);
+
+    return euler;
+}
+
+// responsible for saving the map
 void StoreMap(ORB_SLAM2::System &SLAM){
     std::vector<ORB_SLAM2::MapPoint*> mapPoints = SLAM.GetMap()->GetAllMapPoints();
     std::ofstream pointData;
@@ -150,4 +216,177 @@ void StoreMap(ORB_SLAM2::System &SLAM){
         }
     }
     pointData.close();
+    NavigateToDoor();
+}
+
+// responsible for navigating the drone through the door
+void NavigateToDoor()
+{
+    ifstream is("/tmp/pointData.csv", ifstream::binary);
+    float degToDoor = findDoor(is);
+    cout << "degree to the door: " << degToDoor << endl;
+
+    tello.SendCommand("cw " + std::to_string(degToDoor - euler.at<double>(2)));
+    while (!tello.ReceiveResponse()) { sleep(0.2);}
+    sleep(0.5);
+    tello.SendCommand("forward 100");
+    while (!tello.ReceiveResponse()) { sleep(0.2);}
+    sleep(0.5);
+    moving = false;
+}
+
+#define _USE_MATH_DEFINES
+
+using std::vector;
+
+/*****************************
+        Read CSV file
+*****************************/
+enum class CSVState {
+    UnquotedField,
+    QuotedField,
+    QuotedQuote
+};
+
+vector<std::string> readCSVRow(const std::string& row) {
+    CSVState state = CSVState::UnquotedField;
+    vector<std::string> fields{ "" };
+    size_t i = 0; // index of the current field
+    for (char c : row) {
+        switch (state) {
+        case CSVState::UnquotedField:
+            switch (c) {
+            case ',': // end of field
+                fields.push_back(""); i++;
+                break;
+            case '"': state = CSVState::QuotedField;
+                break;
+            default:  fields[i].push_back(c);
+                break;
+            }
+            break;
+        case CSVState::QuotedField:
+            switch (c) {
+            case '"': state = CSVState::QuotedQuote;
+                break;
+            default:  fields[i].push_back(c);
+                break;
+            }
+            break;
+        case CSVState::QuotedQuote:
+            switch (c) {
+            case ',': // , after closing quote
+                fields.push_back(""); i++;
+                state = CSVState::UnquotedField;
+                break;
+            case '"': // "" -> "
+                fields[i].push_back('"');
+                state = CSVState::QuotedField;
+                break;
+            default:  // end of quote
+                state = CSVState::UnquotedField;
+                break;
+            }
+            break;
+        }
+    }
+    return fields;
+}
+
+/// Read CSV file, Excel dialect. Accept "quoted fields ""with quotes"""
+vector<vector<double>> readCSV(std::istream& in) {
+    vector<vector<std::string>> table;
+    std::string row;
+    while (!in.eof()) {
+        std::getline(in, row);
+        if (in.bad() || in.fail()) {
+            break;
+        }
+        auto fields = readCSVRow(row);
+        table.push_back(fields);
+    }
+    // convert from an vector of strings to vector of doubles
+    vector<vector<double>> points;
+    for (auto& row : table)
+    {
+        double x = std::stod(row[0]);
+        double y = std::stod(row[1]);
+        double z = std::stod(row[2]);
+        vector<double> values = { x, y, z };
+        points.push_back(values);
+        //double dist = sqrt(x * x + y * y + z * z);
+        //std::cout << x << " " << y << " " << z << " " << dist << std::endl;
+    }
+    return points;
+}
+
+/*****************************
+    Find door algorithem
+*****************************/
+
+double findDoor(std::istream& file)
+{
+    // constants that can be optimaized
+    int numof_slices = 60;
+    int overlapping = 5;
+    // read the points in the CSV file
+    auto points = readCSV(file);
+    // we will look at the points "from above", ignoring the "y" value
+    // calculate degrees and normalize them to be between 0 and 1
+    vector<double> alpha;
+    for (auto & row : points)
+    {
+        double a = atan2(row[0], row[2]);
+        a = (a + M_PI) / (2 * M_PI);
+        alpha.push_back(a);
+    }
+    // find the degree that gives the max standard deviation
+    vector<double> sums(numof_slices, 0);       // the sum of the distences
+    vector<double> sums2(numof_slices, 0);      // the sum of the dis
+    vector<int> counts(numof_slices, 0);        // the number of points in the slice
+    int num_points = points.size();
+    for (int i = 0; i < num_points; i++)
+    {
+        int cell = int(alpha[i] * numof_slices);
+        double x = points[i][0];
+        x *= x;
+        double z = points[i][2];
+        z *= z;
+        double dist = sqrt(x + z);
+        counts[cell] += 1;
+        sums[cell] += dist;
+        sums2[cell] += dist * dist;
+    }
+    // calculate standard deviation for each cell
+    vector<double> stds(numof_slices, 0);
+    for (int i = 0; i < numof_slices; i++)
+    {
+        if (counts[i] != 0)
+        {
+            double avg = sums[i] / counts[i];
+            stds[i] = sqrt(sums2[i] / counts[i] - avg * avg);
+        }
+    }
+    // "smooth" the values to find destinct maximum
+    vector<double> stds2(numof_slices, 0);
+    for (int i = 0; i < numof_slices; i++)
+    {
+        stds2[i] = stds[i];
+        for (int r = 0; r < overlapping; r++)
+        {
+            stds2[i] += (stds[(i + r) % numof_slices] + stds[(i - r + numof_slices) % numof_slices]);
+        }
+    }
+    // find the place of the maximum standard deviation
+    int max = 0;
+    for (int i = 1; i < numof_slices; i++)
+    {
+        if (stds2[max] < stds2[i])
+            max = i;
+    }
+    // revert changes made to the degree
+    double teta = (double(max) / numof_slices) * (2 * M_PI) - M_PI;
+    // convert from radians to degrees
+    teta = teta * 180 / M_PI;
+    return teta;
 }
